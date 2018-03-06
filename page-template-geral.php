cba9e3cf58b84dbec23c66ab36dff7fe1c9d5417
Thiago <souza.ty@gmail.com>
2018-03-06 20:13:29 -0300

    Update

--- page-template-geral.php
+++ page-template-geral.php
@@ -4,12 +4,6 @@ Template name: Página Genérica
 */
  ?>
 <?php get_header(); ?>
-<!--
-Code Header image
-
-<img class="img-responsive" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
--->
-
 <section class="wrapper">
     <div class="container">
         <main>
